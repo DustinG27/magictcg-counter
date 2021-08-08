01e3f4a62833efe460e19e39e5ec1edc568ddde6
dustin george <DustinG27@gmail.com>
2021-08-07 23:05:33 -0400

    connected buttons to js

---
+++ app.js
@@ -0,0 +1,31 @@
+// connect life gain and loss buttons
+const lifeLoss = document.querySelector("#lossone");
+const lifeLossTwo = document.querySelector("#losstwo");
+const lifeGain = document.querySelector("#gainone");
+const lifeGainTwo = document.querySelector("#gaintwo");
+
+// connecting total lives
+const playerOne = document.querySelector("#playerone");
+const playerTwo = document.querySelector("#playertwo");
+
+// setting life totals to 0
+playerOne = 0;
+playerTwo = 0;
+
+
+lifeGain.addEventListener('click', function() {
+    alert("click");
+})
+
+lifeLoss.addEventListener('click', function() {
+    alert("click");
+})
+
+lifeGainTwo.addEventListener('click', function() {
+    alert("click");
+})
+
+lifeLossTwo.addEventListener('click', function() {
+    alert("click");
+})
+
