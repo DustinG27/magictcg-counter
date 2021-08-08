6a4b4704f9834de4282edbce9712797ee766d2d3
dustin george <DustinG27@gmail.com>
2021-08-08 11:19:26 -0400

    connected game type to life value

--- app.js
+++ app.js
@@ -4,9 +4,52 @@ const lifeLossTwo = document.querySelector("#losstwo");
 const lifeGain = document.querySelector("#gainone");
 const lifeGainTwo = document.querySelector("#gaintwo");
 
+//game type button and reset button link
+const resetBtn = document.querySelector("#reset");
+const gameType = document.querySelector("#game-type");
+
 // connecting total lives
 const playerOne = document.querySelector("#playerone");
 const playerTwo = document.querySelector("#playertwo");
+
+// game state functionality
+gameType.addEventListener("change", function() {
+    // let standard = 20;
+    // let brawl = 25;
+    // let edh = 30;
+    // let edh2 = 40;
+
+   switch (gameType.value) {
+       case "standard":
+           counterOne = 20;
+           counterTwo = 20;
+           playerOne.textContent = counterOne;
+        playerTwo.textContent = counterTwo;
+           break;
+        case "brawl":
+            counterOne = 25;
+            counterTwo = 25;
+            playerOne.textContent = counterOne;
+            playerTwo.textContent = counterTwo;
+            break;
+        case "edh":
+            counterOne = 30;
+            counterTwo = 30;
+            playerOne.textContent = counterOne;
+            playerTwo.textContent = counterTwo;
+            break;
+        case "edh-multiple":
+            counterOne = 40;
+            counterTwo = 40;
+            playerOne.textContent = counterOne;
+            playerTwo.textContent = counterTwo;
+            break;
+        default:
+            counterOne = 0;
+            counterTwo = 0;
+            break;
+   };
+});
 
 // setting life totals to 0
 counterOne = 0;
@@ -37,6 +80,11 @@ lifeLossTwo.addEventListener('click', function() {
     playerTwo.textContent = counterTwo;
 })
 
-function gameState() {
-    
-}
+// reset button functionality
+resetBtn.addEventListener("click", function() {
+    counterOne = 0;
+    counterTwo = 0;
+    playerOne.textContent = counterOne;
+    playerTwo.textContent = counterTwo;
+})
+
